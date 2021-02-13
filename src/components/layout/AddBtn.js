be2c238e2c2ec518bfec78061872860ece55b1f3
Chris Paterson <chrispaterson22@gmail.com>
2021-02-13 13:31:51 +0000

    AddBtn & AddLogModal components complete

--- src/components/layout/AddBtn.js
+++ src/components/layout/AddBtn.js
@@ -15,7 +15,12 @@ const AddBtn = () => {
 						href='#tech-list-modal'
 						className='btn-floating green modal-trigger'
 					>
-						{/* <i className='material-icons'>TODO</i> */}
+						<i className='material-icons'>person</i>
+					</a>
+				</li>
+				<li>
+					<a href='#tech-modal' className='btn-floating red modal-trigger'>
+						<i className='material-icons'>person_add</i>
 					</a>
 				</li>
 			</ul>
