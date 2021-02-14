48ac57e760c88cb1f8c1271c65d24da02e69f483
Chris Paterson <chrispaterson22@gmail.com>
2021-02-14 11:44:10 +0000

    Delete Techs, app/project complete (for now)

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -4,7 +4,6 @@ import {
 	DELETE_TECH,
 	SET_LOADING,
 	TECHS_ERROR,
-	ADD_LOG,
 } from './types'
 
 // Get Techs from server
@@ -44,6 +43,27 @@ export const addTech = tech => async dispatch => {
 		dispatch({
 			type: ADD_TECH,
 			payload: data,
+		})
+	} catch (err) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: err.response.statusText,
+		})
+	}
+}
+
+// Delete Tech
+export const deleteTech = id => async dispatch => {
+	try {
+		setLoading()
+
+		await fetch(`/techs/${id}`, {
+			method: 'DELETE',
+		})
+
+		dispatch({
+			type: DELETE_TECH,
+			payload: id,
 		})
 	} catch (err) {
 		dispatch({
